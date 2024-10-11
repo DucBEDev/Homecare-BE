cf08eff28c37f2eb39b6a00985a4785ec7786404
DucBEDev <ducpham160604@gmail.com>
2024-10-11 21:25:30 +0700

    Update generalSetting model and API

--- controller/generalSetting.controller.js
+++ controller/generalSetting.controller.js
@@ -6,7 +6,7 @@ const systemConfig = require("../config/system");
 
 // [GET] /admin/generalSettings
 module.exports.index = async (req, res) => {
-    const generalSetting = await GeneralSetting.findOne({ _id: "6707dc930db8d4059e0bbd65" });
+    const generalSetting = await GeneralSetting.findOne({ id: "generalSetting" });
 
     res.render("pages/generalSetting/index", {
         pageTitle: "Cài đặt chung",
@@ -16,7 +16,7 @@ module.exports.index = async (req, res) => {
 
 // [POST] /admin/generalSettings/update
 module.exports.update = async (req, res) => {
-    await GeneralSetting.updateOne({ _id: "6707dc930db8d4059e0bbd65" }, req.body);
+    await GeneralSetting.updateOne({ id: "generalSetting" }, req.body);
 
     req.flash("success", "Cập nhật thành công!");
     res.redirect("back");
