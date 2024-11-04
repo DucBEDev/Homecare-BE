add6f2af32c5a9babaf7f8f114b37f763c9e8391
DucBEDev <ducpham160604@gmail.com>
2024-11-04 10:47:17 +0700

    Fix CORS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 // Config CORS to connect FE and BE
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://homecare-beta.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
